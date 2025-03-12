7be1e9d0f7c42bc6c8deab31e6ac44d2d14678b0
n01596114 <n01596114@humbermail.ca>
2025-03-11 23:34:49 -0400

    current progress

---
+++ next.config.js
@@ -0,0 +1,27 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+  images: {
+    domains: [
+      "i.scdn.co",
+      "t.scdn.co",
+      "newjams-images.scdn.co",
+      "dailymix-images.scdn.co",
+      "seed-mix-image.spotifycdn.com",
+      "wrapped-images.spotifycdn.com",
+      "charts-images.scdn.co",
+      "daily-mix.scdn.co",
+      "blend-playlist-covers.spotifycdn.com",
+      "mosaic.scdn.co",
+      "mixed-media-images.spotifycdn.com",
+      "lineup-images.scdn.co",
+      "thisis-images.scdn.co",
+      "image-cdn-ak.spotifycdn.com",
+      "image-cdn-fa.spotifycdn.com"
+    ],
+  },
+};
+
+module.exports = nextConfig;
